908bab4ce1f2fb50fdbf3f99087d8df600b2d31f
tannerskluz <t.skluzack1599@gmail.com>
2021-02-18 18:51:30 -0600

    added dbug option

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import cv2
 import numpy as np
 import sys
+import time
 from pathlib import Path
 import preprocess as preprocess
 import bfs_solve as bfs
@@ -22,7 +23,7 @@ def draw_path(img, path, thickness=5):
 if __name__ == "__main__":
 	#check that command line argument is taken in
 	if len(sys.argv) < 4:
-		print("How to run: \npython3 main.py ~~path_to_image~~ ~xstart,ystart~ ~xend,yend~")
+		print("How to run: \npython3 main.py ~~path_to_image~~ ~xStart,yStart~ ~xEnd,yEnd~ ~DebugOption(0/1)~")
 		exit()
 	image_path = sys.argv[1]
 	start = sys.argv[2]
@@ -36,9 +37,18 @@ if __name__ == "__main__":
 		print("How to run: \npython3 main.py ~~path_to_image~~ ~xstart,ystart~ ~xend,yend~")
 		exit()
 
+	debug = False
+	try:
+		debug_option = int(sys.argv[4])
+		if debug_option	== 1:
+			debug = True
+	except:
+		pass
+
 	print("Input file: ", image_path)
-	print("Start coordinate: (" + str(x_start) + ', ' + str(y_start) + ')')
-	print("End coordinate: (" + str(x_end) + ', ' + str(y_end) + ')')
+	if debug:
+		print("Start coordinate: (" + str(x_start) + ', ' + str(y_start) + ')')
+		print("End coordinate: (" + str(x_end) + ', ' + str(y_end) + ')')
 	file_check = Path(image_path)
 
 	#check that file exits
@@ -48,12 +58,10 @@ if __name__ == "__main__":
 
 	image_array_color = cv2.imread(image_path,1)
 	height, width, channels = image_array_color.shape
-	#cv2_to_PIL(image_array_color).show()
+	if debug:
+		print("Image dimensions: (" + str(width) + ', ' + str(height) + ')')
 
-	#returns preprocessed image in binary output form
 	processed_bits = preprocess.regular_threshold(image_path)
-	# view = cv2_to_PIL(processed_bits)
-	# view.show()
 
 	path_pixels = 6
 	circle_r = 6
@@ -64,17 +72,27 @@ if __name__ == "__main__":
 		path_pixels = 8
 		circle_r = 8
 
+	if debug:
+		print('Path pixels: ', path_pixels)
+		print('Circle pixels: ', circle_r)
 
+	start_time = time.time()
 	solve_path = bfs.find_shortest_path_bfs(processed_bits, x_start, y_start, x_end, y_end)
+	end_time = time.time()
+
+	print('Solution found in ' + str(end_time-start_time) + ' seconds')
+
 	draw_path(image_array_color, solve_path, path_pixels)
 	cv2.circle(image_array_color, (x_start, y_start), circle_r, (0,200,40), -1)
 	cv2.circle(image_array_color, (x_end, y_end), circle_r, (0,0,255), -1)
 
 	maze_w_solution = cv2_to_PIL(image_array_color)
-	# maze_w_solution.show()
-
-	#save image
-	save_path = 'maze_images/generated_solutions/' + image_path.split('/')[-1].split('.')[0] + '_solution.png'
-	print("Saving image under: ", save_path)
-	maze_w_solution.save(save_path)
+	
+	if debug:
+		maze_w_solution.show()
+	else:
+		#save image
+		save_path = 'maze_images/generated_solutions/' + image_path.split('/')[-1].split('.')[0] + '_solution.png'
+		print("Saving image under: ", save_path)
+		maze_w_solution.save(save_path)
 
