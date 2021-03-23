cefe26990c31227089c4f4d055e984f4f23f4fb1
tannerskluz <t.skluzack1599@gmail.com>
2021-03-22 22:06:12 -0500

    added Astar implementation

--- Vertex.py
+++ Vertex.py
@@ -1,8 +1,14 @@
 class Vertex:
-	def __init__(self, x_coordinate, y_coordinate):
+	def __init__(self, x_coordinate, y_coordinate, fscore = float('inf'), gscore = float('inf')):
 		self.x = x_coordinate
 		self.y = y_coordinate
 		self.xParent = None
 		self.yParent = None
 		self.processed = False
 		self.queueIndex = None
+		self.condition = None
+		#additions for aStar
+		self.fscore = fscore
+		self.gscore = gscore
+		self.closed = False
+		self.out_openset = True
