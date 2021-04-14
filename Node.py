f8fd363fa9d90d969cfebf66796fe219437f6df0
tannerskluz <t.skluzack1599@gmail.com>
2021-04-14 00:56:42 -0500

    changes

--- Node.py
+++ Node.py
@@ -9,11 +9,11 @@ class Node:
 
 class NodeBFS(Node):
 	def __init__(self, x, y, x_parent, y_parent):
-		Node.__init__(x, y, x_parent, y_parent)
+		Node.__init__(self, x, y, x_parent, y_parent)
 		self.processed = False
 		self.queue_index = -1
 
-	def get_neighbors
+	# def get_neighbors
 
 
 class NodeAStar(Node):
